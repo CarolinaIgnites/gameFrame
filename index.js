64d69a7339e49285e0aa962366c430b008db5256
kmsalah <salah4501@gmail.com>
2020-04-11 00:42:29 -0700

    forgot comma

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = Object.assign(
 	{},
 	require('./src/js/gameframe'),
-	require('./src/js/interactive-custom')
+	require('./src/js/interactive-custom'),
 	require('./src/js/manifest.json')
 );
